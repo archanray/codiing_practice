ab27fbccb625fc078fba41de5befbe23f4922640
unknown <talk2archan@gmail.com>
2022-10-07 20:17:03 -0400

    new prob

--- longest_substring_wo_rep_char.py
+++ longest_substring_wo_rep_char.py
@@ -1,50 +1,39 @@
 import numpy as np
-import string 
+import string
 
-def searchAround(s, i):
-    length = 0
-    palindrome = ""
-    flag = 0
-    if i == 0 or i == len(s)-1:
-        length = 1
-        palindrome = s[int(i)]
-        return length, palindrome, flag
-    for j in np.arange(0,i+0.5,0.5):
-        base = int(np.ceil(i-j))
-        top = int(np.floor(i+j))
-        # print(base, top)
-        if s[base] == s[top]:
-            palindrome = s[base:top+1]
-            length = len(palindrome)
+def go_back_and_find(s: list, start2: int, start1: int):
+    hash2 = []
+    length2 = 0
+    for i in range(start2, start1-1, -1):
+        if s[i] not in hash2:
+            hash2.append(s[i])
+            length2 += 1
         else:
             break
-        if top == len(s)-1:
-            flag = 1
-            break
-    return length, palindrome, flag
+    start2 = i+1
+    return start2, length2, hash2
+    
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        s = list(s)
+        length1 = 0
+        length2 = 0
+        start1 = 0
+        start2 = 0
+        hash1 = []
+        hash2 = []
+        s = list(s)
+        for i in range(len(s)):
+            if s[i] not in hash2:
+                hash2.append(s[i])
+                length2 += 1
+                if length1 <= length2:
+                    hash1 = hash2
+                    length1 = length2
+                    start1 = start2
+            else:
+                start2, length2, hash2 = go_back_and_find(s, i, start1)
+            # print(length1, length2, start1, start2)
+            pass
+        return length1
 
-# LoLS:: length of longest subsequence O(1) space requirement
-def centerLoLS(s):
-    strLen = 0
-    reqPal = ""
-    flag = 0
-    for i in np.arange(0,len(s)-0.5, 0.5):
-        # print(s, i)
-        length, maxpal, flag = searchAround(s,i)
-        # print("algo output:", length, maxpal)
-        if length >= strLen:
-            # print("HERE", length, maxpal)
-            strLen = length
-            reqPal = maxpal
-        if flag:
-            return strLen, reqPal        
-        # print("==========================================")
-    return strLen, reqPal
-
-# print(centerLoLS("babad"))
-# print(centerLoLS("aab"))
-# print(centerLoLS("trinitrophenylmethylnitramine"))
-print(centerLoLS("trinityeeree"))
-# print(centerLoLS("aacabdkacaa"))
-# print(centerLoLS("hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
-# print(centerLoLS("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"))
