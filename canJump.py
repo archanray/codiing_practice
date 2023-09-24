d683cff5d7b3437b4354d92ff74a253fa9cb3d96
unknown <talk2archan@gmail.com>
2023-09-23 21:33:53 -0400

    comments for canjump

--- canJump.py
+++ canJump.py
@@ -1,10 +1,20 @@
 class Solution:
     def canJump(self, nums: list[int]) -> bool:
+        """
+        from any position see where you can reach.
+        if you cant improve reah and 
+        your id is less than max reach return false
+        else continue
+        if total reach is geq final element
+        return true
+        if for loop end return false
+        compute current_reach as i+nums[i]
+        max_reach = max(max_reach, current_reach)
+        """
         if len(nums) == 1:
             return True
         max_reach = 0
         nmo = len(nums)-1
-        # BFS
         for i in range(nmo):
             current_reach = nums[i]+i
             if max_reach == current_reach and i >= current_reach:
