f4bfb520bfc2ea421889dd71f73a1c3ea1e63acc
archanray <ray@cs.umass.edu>
2023-11-02 16:49:07 -0400

    size of connected components

--- numProvinces.py
+++ numProvinces.py
@@ -1,15 +1,25 @@
 class Solution():
 	def dfs(self, start):
-		if 
+		if self.visited[start] == 0:
+			self.visited[start] = 1
+			for j in range(self.n):
+				if self.isConnected[start][j] == 1:
+					self.dfs(j)
 	def findCircleNum(self, isConnected):
+		# finding connected components in a graph
 		self.isConnected = isConnected
-		self.output = []
 		self.n = len(isConnected)
-		self.visited = [k]
+		self.visited = [0 for _ in range(self.n)]
 		if self.n == 0:
-			return []
-		self.dfs(0, )
-		return None
+			return 0
+		count = 0
+		for i in range(self.n):
+			if self.visited[i] == 0:
+				# print(i, self.visited)
+				self.dfs(i)
+				count += 1
+		return count
 
+inputs = [[1,0,0],[0,1,0],[0,0,1]]
 q = Solution()
-print(q.simplifyPath(inputs))
+print(q.findCircleNum(inputs))
