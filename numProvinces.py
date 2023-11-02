99569cc2afefe887a207523818ec11bd6c53ff19
unknown <talk2archan@gmail.com>
2023-11-02 16:03:55 -0400

    checks

---
+++ numProvinces.py
@@ -0,0 +1,15 @@
+class Solution():
+	def dfs(self, start):
+		if 
+	def findCircleNum(self, isConnected):
+		self.isConnected = isConnected
+		self.output = []
+		self.n = len(isConnected)
+		self.visited = [k]
+		if self.n == 0:
+			return []
+		self.dfs(0, )
+		return None
+
+q = Solution()
+print(q.simplifyPath(inputs))
--- simplifyPath.py
+++
@@ -1,7 +0,0 @@
-class Solution():
-	def simplifyPath(self, path):
-		return None
-
-inputs = "/home/"
-q = Solution()
-print(q.simplifyPath(inputs))
