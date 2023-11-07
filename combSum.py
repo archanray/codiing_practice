70798c4566f7832e0d6307208d7b3d00fc61a63c
archanray <ray@cs.umass.edu>
2023-11-06 20:58:44 -0500

    DFS practice

--- combSum.py
+++ combSum.py
@@ -1,9 +1,28 @@
 class Solution:
-    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+    def dfs(self, idx, targetSum, path):
+        if targetSum < 0:
+            return
+        if targetSum == 0:
+            self.output.append(path)
+            return
+        for i in range(idx, self.n):
+            q = self.candidates[i]
+            # set call index as i if reapts are allowed, else do i+1
+            self.dfs(i, targetSum - q, path+[q])
+
+    def combinationSum(self, candidates, target):
+        self.candidates = candidates
+        self.n = len(self.candidates)
+        self.output = []
+        self.dfs(0, target, [])
+        return self.output
+
+    def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
         result = []
         self.dfs(candidates, target, [], result)
         return result
-    def dfs(self, nums, target, path, result):
+
+    def dfs2(self, nums, target, path, result):
         if target < 0:
             return
         if target == 0:
@@ -15,4 +34,4 @@ class Solution:
 
 
 q = Solution()
-print(q.combinationSum([2], 1))
+print(q.combinationSum([2,3,6,7], 7))
--- combinations.py
+++ combinations.py
@@ -1,5 +1,20 @@
 class Solution:
-    def combine(self, n: int, k: int) -> list[list[int]]:
+    def dfs(self, i, path):
+        if len(path) > self.k:
+            return
+        if len(path) == self.k:
+            self.output.append(path)
+        for i in range(i, self.n):
+            self.dfs(i+1, path+[i+1])
+
+    def combine(self, n, k):
+        self.output = []
+        self.n = n
+        self.k = k
+        self.dfs(0, [])
+        return self.output
+
+    def combine2(self, n: int, k: int) -> list[list[int]]:
         """
         given n, k,
         return all possible combinations of k numbers
@@ -18,4 +33,4 @@ class Solution:
         return output
 
 q = Solution()
-print(q.combine(4,2))
+print(q.combine(4,3))
