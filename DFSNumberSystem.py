242e17cc9e81a9f097d92881b74a4f17d78fb9f1
unknown <talk2archan@gmail.com>
2024-02-03 13:54:50 -0500

    saturday cehck

---
+++ DFSNumberSystem.py
@@ -0,0 +1,12 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def DFS(self, root):
+        return None
+    def sumNumbers(self, root):
+        self.DFS(root, "")
+        return None
--- longestConseqSeqDis.py
+++ longestConseqSeqDis.py
@@ -1,3 +1,17 @@
 class Solution:
-    def longestConsecutive(self, list):
-        return None
+    def longestConsecutive(self, nums):
+        uniques = set(nums)
+        max_length = 0
+        
+        while uniques:
+            low = high = uniques.pop()
+            
+            while low-1 in uniques or high+1 in uniques:
+                if low-1 in uniques:
+                    uniques.remove(low-1)
+                    low -= 1
+                if high+1 in uniques:
+                    uniques.remove(high+1)
+                    high += 1
+            max_length = max(high-low+1, max_length)
+        return max_length
