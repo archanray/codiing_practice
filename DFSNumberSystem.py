644d4479bb3d5409e08b96f19c23ac9296b3cc97
archanray <ray@cs.umass.edu>
2024-02-04 14:39:07 -0500

    check DFS

--- DFSNumberSystem.py
+++ DFSNumberSystem.py
@@ -5,8 +5,26 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def DFS(self, root):
-        return None
+    def DFS(self, root, path):
+        if root is None:
+            return path
+        else:
+            path = path+str(root.val)
+        path = self.DFS(root.left, path)
+        path = self.DFS(root.right, path)
+        return path
+    
     def sumNumbers(self, root):
-        self.DFS(root, "")
-        return None
+        path = self.DFS(root, "")
+        return path
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+q = Solution()
+print("checking DFS:", q.sumNumbers(root))
