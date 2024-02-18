82774f3e116f540ffaffeb2632805572d7704071
archanray <ray@cs.umass.edu>
2024-02-18 17:28:31 -0500

    subtreesum

--- split_tree.py
+++ split_tree.py
@@ -1,37 +1,46 @@
 # given a binary tree, each node has a value which represents 'water flow', separate the tree into 2 trees from the root, so that the difference between sum of water flows of each tree is minimal. this input is was 2 equal length arrays, first one described tree structure where arr[i] was the parent of this node, second one had each nodes value
 
-# store sum of subtree in val
-# then check the difference of any node to its sub node
-# find the min and return
+# compute sum of subtree store in val
+# total sum is stored in the root
+# for any node, abs((totalSum - subtree sum) - subtreeSum) is the val we need to track!
 
 class TreeNode:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+        self.postOrderSum = 0
 
 class Solution:
-    def postorder(self, root):
+    def totalSum(self, root):
+        # compute using post order traversal
         if root == None:
             return 0
-        root.val += self.postorder(root.left) + self.postorder(root.right)
-        return root.val
+        root.postOrderSum = self.totalSum(root.left) + self.totalSum(root.right) + root.val
+        return root.postOrderSum
     
-    def preorder(self, root):
+    def findMinDiffSingle(self, root, val):
         if root == None:
-            return 
+            return
+        leftDiff = abs(val+root.postOrderSum - 2*root.left.postOrderSum)
+        rightDiff = abs(val+root.postOrderSum - 2*root.right.postOrderSum)
         
-        leftDiff = abs(root.left.val - (root.val-root.left.val))
-        rightDiff = abs(root.right.val - (root.val-root.right.val))
-        
-        self.minDiff = min(self.minDiff, min(leftDiff, rightDiff))
+        self.minDiff = min(self.minDiff, leftDiff, rightDiff)
+        return
+    
+    def findMinDiff(self, root):
+        if root == None:
+            return
+        self.findMinDiffSingle(root, 0)
+        self.findMinDiffSingle(root.left, root.right.postOrderSum+root.val)
+        self.findMinDiffSingle(root.right, root.left.postOrderSum+root.val)
         return
     
     def minAbsDiff(self, root):
         import sys
         self.minDiff = sys.maxsize
-        self.postorder(root)
-        self.preorder(root)
+        self.totalSum(root)
+        self.findMinDiff(root)
         return self.minDiff
 
 # Construct the tree
